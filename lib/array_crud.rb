f336c5efc6d3ef6f22cf02a09ea2633329dcbd07
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-12 21:07:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  # my_array = ["earth", "air", 'fire', 'water']
-  (array, element) << "Metal"
+  array << element
   
 end
 
